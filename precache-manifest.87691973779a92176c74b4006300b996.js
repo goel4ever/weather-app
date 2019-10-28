df6a6603acd06214cc0c14afae427e4fe257a483
Anshul Goel <anshul@zenabi.com>
2019-10-27 23:33:05 -0400

    Updates

---
+++ precache-manifest.87691973779a92176c74b4006300b996.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf64d0e93f8493baa8bf0c7f9ca9c39",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "2986c6edc76123ff697e",
+    "url": "/weather-app/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "c7c5368d5ffe691a9216",
+    "url": "/weather-app/static/css/main.e085a1ac.chunk.css"
+  },
+  {
+    "revision": "2986c6edc76123ff697e",
+    "url": "/weather-app/static/js/2.9365b462.chunk.js"
+  },
+  {
+    "revision": "c7c5368d5ffe691a9216",
+    "url": "/weather-app/static/js/main.332fa0f5.chunk.js"
+  },
+  {
+    "revision": "b0ae6103f372a3fc325e",
+    "url": "/weather-app/static/js/runtime-main.9bf36d48.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/weather-app/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/weather-app/static/media/logo.25bf045c.svg"
+  }
+]);
